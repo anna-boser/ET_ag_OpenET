c9791428d61765ec80e4197a9d83b9a407db7c40
anna-boser <46419226+anna-boser@users.noreply.github.com>
2022-11-07 18:15:27 -0800

    bug fix

--- code/1_generate_datasets/1_intermediate/4_PET.R
+++ code/1_generate_datasets/1_intermediate/4_PET.R
@@ -12,6 +12,7 @@ library(ncdf4) # package for netcdf manipulation
 library(raster) # package for raster manipulation
 library(rgdal) # package for geospatial analysis
 library(ggplot2) # package for plotting
+library(parallel)
 
 source("file_paths.R")
 
