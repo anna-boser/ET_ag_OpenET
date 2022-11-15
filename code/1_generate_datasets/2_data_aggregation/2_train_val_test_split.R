cf37064d64ac843607e06df939c0b4b5adfbfa49
anna-boser <46419226+anna-boser@users.noreply.github.com>
2022-11-15 08:03:58 -0800

    Split the data

---
+++ code/1_generate_datasets/2_data_aggregation/2_train_val_test_split.R
@@ -0,0 +1,55 @@
+# create training, and test splits for use in ML. 
+
+# Training is used to check if we've got a good CV score and 
+# are relatively close to the fallow numbers when we predict over fallow fields
+
+# Test splits will only be used once at the end to generate numbers over the
+# whole ag areas and get a cross-val score. 
+
+# The pseudo-random split will preserve which pixels are considered train or test
+# across natural datasets. 
+# The way it works is all datasets with both an x and a y coordinate with an
+# even decimal in the last place will be included in the training set. 
+# This is about 10% of the data
+# The rest is reserved for test. 
+
+# Anna Boser 11/14/2021
+
+library(here)
+library(raster)
+library(dplyr)
+library(sf)
+library(fasterize)
+library(data.table)
+library(stringr)
+
+source("file_paths.R")
+
+datasets <- c("fveg.tif", 
+              "cpad.tif", 
+              "cdl.tif")
+
+# condition for inclusion
+check_inc <- function(dim){
+  dim <- as.character(dim)
+  last_dec <- substr(dim, nchar(dim), nchar(dim)) %>% as.numeric()
+  (last_dec%%2) == 0
+}
+
+# split and save function
+split <- function(dataset){
+  df <- fread(here(natural_data_loc, dataset))
+  
+  train <- filter(df, check_inc(x) & check_inc(y))
+  dir.create(here(natural_data_loc, "train"))
+  fwrite(train, here(natural_data_loc, "train", dataset), append=FALSE)
+  
+  test <- filter(df, !(check_inc(x) & check_inc(y)))
+  dir.create(here(natural_data_loc, "test"))
+  fwrite(train, here(natural_data_loc, "test", dataset), append=FALSE)
+}
+
+# apply function to all datasets
+for (d in datasets){
+  split(d)
+}
